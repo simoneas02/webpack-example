04a1d555cf3f47ef3adb5191f94aa8a6edbe55d2
simoneas02 <simoneas02@hotmail.com>
2020-02-22 16:07:47 -0300

    add redux

---
+++ src/redux-101.js
@@ -0,0 +1,60 @@
+import { createStore } from 'redux'
+
+// this.setState(prev => prev) // the createStore is similary to setState
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      const incrementBy =
+        typeof action.incrementBy === 'number' ? action.incrementBy : 1
+      return {
+        count: state.count + incrementBy
+      }
+    case 'DECREMENT':
+      const decrementBy =
+        typeof action.decrementBy === 'number' ? action.decrementBy : 1
+      return {
+        count: state.count - decrementBy
+      }
+    case 'RESET':
+      return {
+        count: action.count
+      }
+    case 'SET':
+      return {
+        count: 0
+      }
+    default:
+      return state
+  }
+})
+
+const unsubscribe = store.subscribe(() =>
+  console.log('store', store.getState())
+)
+
+store.dispatch({
+  type: 'INCREMENT',
+  incrementBy: 5
+})
+
+store.dispatch({
+  type: 'INCREMENT'
+})
+
+store.dispatch({
+  type: 'RESET'
+})
+
+store.dispatch({
+  type: 'DECREMENT'
+})
+
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 10
+})
+
+store.dispatch({
+  type: 'SET',
+  count: 101
+})
